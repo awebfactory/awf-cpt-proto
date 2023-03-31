776b3081909d23da41322d77c5fbfef87c47b3e7
victorkane <victorkane@gmail.com>
2023-03-31 12:31:45 -0300

    build: scaffold project custom post type and description custom field in block plugin

--- wp-content/plugins/awf-cpt-proto/awf-cpt-proto.php
+++ wp-content/plugins/awf-cpt-proto/awf-cpt-proto.php
@@ -21,6 +21,38 @@
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function awebfactory_awf_cpt_proto_block_init() {
-	register_block_type( __DIR__ . '/build' );
+  register_block_type( __DIR__ . '/build' );
+  register_post_type(
+    'project',
+    array(
+      'labels'       => array(
+        'name'          => __( 'Projects', 'tutorial' ),
+        'singular_name' => __( 'Project', 'tutorial' ),
+      ),
+      'public'       => true,
+			'menu_icon'    => 'dashicons-schedule',
+      'has_archive'  => true,
+      'show_in_rest' => true,
+      'supports'     => array(
+        'title',
+        'editor',
+        'thumbnail',
+        'excerpt',
+        'custom-fields',
+      ),
+    )
+  );
+  register_post_meta(
+    'project',
+    'description',
+    array(
+      'show_in_rest'       => true,
+      'single'             => true,
+      'type'               => 'string',
+      'sanitize_callback'  => 'wp_kses_post',
+    )
+  );
 }
 add_action( 'init', 'awebfactory_awf_cpt_proto_block_init' );
+
+
