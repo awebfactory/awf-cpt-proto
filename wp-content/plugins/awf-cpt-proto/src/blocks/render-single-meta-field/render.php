cc012afd1f97781134aa6804df706a52c507d3d0
victorkane <victorkane@gmail.com>
2023-04-09 16:38:06 -0300

    build(awf-cpt-proto): scaffold rendering current post and post meta data in block editor single-project template

--- wp-content/plugins/awf-cpt-proto/src/blocks/render-single-meta-field/render.php
+++ wp-content/plugins/awf-cpt-proto/src/blocks/render-single-meta-field/render.php
@@ -1,3 +1,16 @@
+<?php
+  $post = $GLOBALS['post'];
+?>
 <p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Render Single Meta Field – hello from a dynamic block on the front end!', 'render-single-meta-field' ); ?>
+	<?php esc_html_e( 'Render Single Meta Field – hola from a dynamic block on the front end!', 'render-single-meta-field' ); ?>
+  <?php 
+	  global $post;
+		$the_meta = get_post_meta($post->ID);
+		echo '<h3>' . 'Post ID: ' . '</h3>'; 
+		echo '<pre>' . $post->ID . '</pre>';
+		echo '<h3>' . 'Post Object: ' . '</h3>'; 
+		echo '<pre>' . var_export($post, true) . '</pre>';
+		echo '<h3>' . 'Post Meta Object: ' . '</h3>'; 
+		echo '<pre>' . var_export($the_meta, true) . '</pre>';
+	?>
 </p>
