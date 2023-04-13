13d874fd3b45fd79125459597e4eea2b6080a518
victorkane <victorkane@gmail.com>
2023-04-13 08:43:08 -0300

    build(render-single-meta-field): more approaches to listing custom post meta fields for project in single theme template

--- wp-content/plugins/awf-cpt-proto/src/blocks/render-single-meta-field/edit.js
+++ wp-content/plugins/awf-cpt-proto/src/blocks/render-single-meta-field/edit.js
@@ -15,6 +15,8 @@ import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
 
 import apiFetch from "@wordpress/api-fetch";
 
+import { useSelect } from "@wordpress/data";
+
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
  * Those files can contain any CSS code that gets applied to the editor.
@@ -33,7 +35,7 @@ import "./editor.scss";
  */
 export default function Edit() {
 	// Replace 'your_post_type_slug' with the slug of your custom post type
-	const postTypeSlug = "project";
+	//  const postTypeSlug = "project";
 	let projectMeta = new Array();
 	let projectMetaSize = 0;
 	const metaKeys = new Set();
@@ -66,6 +68,154 @@ export default function Edit() {
 		});
 	console.log("projectMeta", projectMeta);
 	console.log("projectMeta Size", projectMetaSize);
+
+	const postTypes = useSelect((select) => {
+		return select("core").getPostTypes();
+	}, []);
+	console.log("postTypes", postTypes);
+
+	const postTypeProject = useSelect((select) => {
+		return select("core").getPostType("project");
+	}, []);
+	console.log("postTypeProject", postTypeProject);
+
+	const theSettings = useSelect((select) => {
+		return select("core/block-editor").getSettings();
+	}, []);
+	console.log("theSettings", theSettings);
+	// output: wp_template
+
+	const theEditedPostType = useSelect((select) => {
+		return select("core/edit-site").getEditedPostType();
+	}, []);
+	console.log("theEditedPostType", theEditedPostType);
+
+	// this is it!!!!
+	const theEditedPostId = useSelect((select) => {
+		return select("core/edit-site").getEditedPostId();
+	}, []);
+	console.log("theEditedPostId", theEditedPostId);
+	// output: awforgtheme//single-project
+
+	const theEditedSettings = useSelect((select) => {
+		return select("core/edit-site").getSettings();
+	}, []);
+	console.log("theEditedSettings", theEditedSettings);
+
+	const theEditorMode = useSelect((select) => {
+		return select("core/edit-site").getEditorMode();
+	}, []);
+	console.log("theEditorMode", theEditorMode);
+
+	const theCurrentPostType = useSelect((select) => {
+		return select("core/editor").getCurrentPostType();
+	}, []);
+	console.log("theCurrentPostType", theCurrentPostType);
+
+	/*
+	 * this works great, next example digs deeper into what we need
+	
+	const currentURL = window.location.href;
+	const urlObject = new URL(currentURL);
+	console.log("urlObject", urlObject);
+	// Get the search (query) part of the URL
+	const search = window.location.search;
+	console.log("search only", search);
+
+	// Remove the leading "?" character
+	const searchWithoutQuestionMark = search.slice(1);
+
+	// Split the search string by "&" to get an array of query parameter strings
+	const queryParamStrings = searchWithoutQuestionMark.split("&");
+
+	// Create an empty object to store the extracted query parameters
+	const queryParams = {};
+
+	// Loop through each query parameter string and extract the key-value pairs
+	queryParamStrings.forEach((paramString) => {
+		const [key, value] = paramString.split("=");
+		queryParams[key] = decodeURIComponent(value);
+	});
+
+	console.log("Query Parameters:", queryParams);
+	*/
+
+	//
+	//
+	// break down PostId
+	//
+	//
+
+	// Get the search (query) part of the URL
+	const search = window.location.search;
+
+	// Remove the leading "?" character
+	const searchWithoutQuestionMark = search.slice(1);
+
+	// Split the search string by "&" to get an array of query parameter strings
+	const queryParamStrings = searchWithoutQuestionMark.split("&");
+
+	// Create an empty object to store the extracted query parameters
+	const queryParams = {};
+
+	// Loop through each query parameter string and extract the key-value pairs
+	queryParamStrings.forEach((paramString) => {
+		const [key, value] = paramString.split("=");
+		queryParams[key] = decodeURIComponent(value);
+	});
+
+	// Extract the postId from the queryParams object
+	const postId = queryParams.postId;
+
+	// Split the postId value by slashes to extract individual parts
+	const postIdParts = postId.split("/");
+
+	console.log("postIdParts:", postIdParts);
+
+	/*
+	 * output to last example
+	postIdParts: 
+Array(3) [ "awforgtheme", "", "single-project" ]
+0: "awforgtheme"
+1: ""
+2: "single-project"
+	 */
+
+	/*
+	 * don't work while editing template `single-project` for example
+	 * 
+	const theCollections = useSelect((select) => {
+		return select("core/blocks").getCollections(true);
+	}, []);
+	console.log("theCollections", theCollections);
+	
+	const theTemplate = useSelect((select) => {
+		return select("core/editor").getTemplate();
+	}, []);
+	console.log("theTemplate", theTemplate);
+
+	const theMeta = useSelect((select) => {
+		return select("core/editor").getEditedPostAttribute("meta");
+	}, []);
+	console.log("theMeta", theMeta);
+
+	const thePermalink = useSelect((select) => {
+		return select("core/editor").getPermalink();
+	}, []);
+	console.log("thePermalink", thePermalink);
+
+
+	
+	 */
+
+	// Get information about all registered custom post types
+	/*
+	const postTypes = select("core").getPostTypes();
+	console.log("postTypes", postTypes);
+	const theMeta = select("core/editor").getEditedPostAttribute("meta");
+	console.log("theMeta", theMeta);
+	*/
+
 	return (
 		<p {...useBlockProps()}>
 			{__(
